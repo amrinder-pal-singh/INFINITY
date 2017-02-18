787d51c5a60fcf00b0434a18b3f5efbbcab5fbfd
amrinder-pal-singh <amrinderpal.singh@outlook.com>
2017-02-18 23:34:24 +0530

    Add project files.

---
+++ INFINITY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INFINITY.Startup))]
+namespace INFINITY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
